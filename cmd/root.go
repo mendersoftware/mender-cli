9d3e3ee60fd1feb2d9c7b1a223a260b9371c9d09
Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>
2018-04-18 09:57:27 +0200

    cmd: new artifacts command with upload subcommand
    changelog: New artifacts command with upload subcommand
    
    Signed-off-by: Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -50,6 +50,7 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	rootCmd.AddCommand(loginCmd)
+	rootCmd.AddCommand(artifactsCmd)
 	CheckErr(rootCmd.Execute())
 }
 
